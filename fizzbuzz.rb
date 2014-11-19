004c9f07e99a0eee97fb680c44e21aa4984d5714
Nathan Menge <mengen@upmc.edu>
2014-11-18 23:05:27 -0500

    Created fizzbuzz as a class, so that there could be persistent state regarding the configuration of what value is returned for zero.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,51 @@
 #!/usr/bin/ruby
 
-def fizzbuzz(x)
-  raise TypeError, "Value \"#{x}\" is not a Fixnum" unless x.is_a?(Fixnum)
-  fizz = ->(x) {x % 3 == 0}
-  buzz = ->(x) {x % 5 == 0}
-  fizzbuzz = ->(x) {fizz[x] && buzz[x]}
-  return case x
-    when fizzbuzz then 'Fizzbuzz'
-    when fizz then 'Fizz'
-    when buzz then 'Buzz'
-    else x
+  # Zero is divisible by 3 and five evenly, as well as every other number
+  # Zero getting Fizzbuzz is therefore no necessarily in line with the 
+  # specifications.  Setting zero_mode to true will cause '0' to be 
+  # returned for '0'.  False will return 'Fizzbuzz'
+
+class FizzBuzz
+  attr_reader :zero_mode
+  
+  SETTING_ENUM = [true, false].freeze
+
+  def initialize(zero_mode=false)
+    if SETTING_ENUM.include?(zero_mode)
+      @zero_mode = zero_mode
+    else
+      @zero_mode = false
+    end
+  end
+
+  def change_mode(setting=nil)
+    if setting.nil?
+      @zero_mode = !zero_mode
+    else
+      @zero_mode = setting if SETTING_ENUM.include?(zero_mode)
+    end
+  end
+
+  def fizzbuzz(x)
+    raise TypeError, "Value \"#{x}\" is not a Fixnum" unless x.is_a?(Fixnum)
+    zero = ->(x) {x == 0}
+    fizz = ->(x) {x % 3 == 0}
+    buzz = ->(x) {x % 5 == 0}
+    fizzbuzz = ->(x) {fizz[x] && buzz[x]}
+    return case x
+      when zero then zero_value
+      when fizzbuzz then 'Fizzbuzz'
+      when fizz then 'Fizz'
+      when buzz then 'Buzz'
+      else x
+    end
+  end
+
+  private
+  def zero_value
+    @zero_mode ? 0 : 'Fizzbuzz'
   end
 end
 
-(-100 .. 100).each {|x| puts "#{fizzbuzz(x)}"}
+#fb = FizzBuzz.new(true)
+#(-100 .. 100).each {|x| puts "#{fb.fizzbuzz(x)}"}
--- fizzbuzz_test.rb
+++ fizzbuzz_test.rb
@@ -3,15 +3,42 @@ require './fizzbuzz'
 
 class FizzbuzzTest < MiniTest::Test
   def test_fizzbuzz
+    fb = FizzBuzz.new
     %w(1 2 4 7 8 11 13 14).each do |x|
-      assert_equal x.to_i, fizzbuzz(x.to_i)
-      assert_equal (x.to_i * -1), fizzbuzz((x.to_i * -1))
-      assert_equal 'Fizz', fizzbuzz(x.to_i * 3)
-      assert_equal 'Buzz', fizzbuzz(x.to_i * 5)
-      assert_equal 'Fizzbuzz', fizzbuzz(x.to_i * 15)
+      assert_equal x.to_i, fb.fizzbuzz(x.to_i)
+      assert_equal (x.to_i * -1), fb.fizzbuzz((x.to_i * -1))
+      assert_equal 'Fizz', fb.fizzbuzz(x.to_i * 3)
+      assert_equal 'Buzz', fb.fizzbuzz(x.to_i * 5)
+      assert_equal 'Fizzbuzz', fb.fizzbuzz(x.to_i * 15)
     end
-    assert_equal 'Fizzbuzz', fizzbuzz(0)
-    assert_raises(TypeError) { fizzbuzz("a string") }
-    assert_raises(TypeError) { fizzbuzz(12.2) }    
+    
+    assert_equal 'Fizzbuzz', fb.fizzbuzz(0), "Zero mode: #{fb.zero_mode}"
+    fb.change_mode
+    assert_equal 0.to_i, fb.fizzbuzz(0)
+
+    assert_raises(TypeError) { fb.fizzbuzz("a string") }
+    assert_raises(TypeError) { fb.fizzbuzz(12.2) }    
+  end
+
+  def test_initialize
+    fb = FizzBuzz.new('tiger')
+    refute fb.zero_mode
+    fb = FizzBuzz.new(true)
+    assert fb.zero_mode
+    fb = FizzBuzz.new(false)
+    refute fb.zero_mode
+  end
+
+  def test_change_mode
+    fb = FizzBuzz.new
+    refute fb.zero_mode
+    fb.change_mode
+    assert fb.zero_mode
+    fb.change_mode
+    refute fb.zero_mode
+    fb.change_mode(true)
+    assert fb.zero_mode
+    fb.change_mode(false)
+    refute fb.zero_mode
   end
 end
